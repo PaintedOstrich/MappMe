161e6e7038146dae9952c837f04f1403d0bfb7cc
Codier <pendyaaa@gmail.com>
2012-03-01 20:57:31 -0600

    Some code clean up

--- MappMe/MainViewController.m
+++ MappMe/MainViewController.m
@@ -14,13 +14,10 @@
     MappMeAppDelegate *delegate;
 }
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+-(IBAction)logoutButtonPressed{
+    [[delegate facebook] logout];
+    [self.navigationController popViewControllerAnimated:YES];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -31,21 +28,7 @@
     // Release any cached data, images, etc that aren't in use.
 }
 
--(IBAction)logoutButtonPressed{
-    [[delegate facebook] logout];
-    [self.navigationController popViewControllerAnimated:YES];
-
-}
 #pragma mark - View lifecycle
-
-/*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView
-{
-}
-*/
-
-
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
