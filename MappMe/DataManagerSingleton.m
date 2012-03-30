1d2489fc28ec3b456588cdb459efd0659196e5ae
Parkour <paintedostrich@gmail.com>
2012-03-30 00:16:37 -0500

    made singleton

--- MappMe/DataManagerSingleton.m
+++ MappMe/DataManagerSingleton.m
@@ -8,6 +8,25 @@
 
 #import "DataManagerSingleton.h"
 
+// If you want to know about the synchronous thread safe structure,
+// Read here: http://blog.jh-lim.com/2011/09/implementing-a-singleton-in-ios/
+
 @implementation DataManagerSingleton
+
+static DataManagerSingleton *mainDataManager = nil;
+
++(DataManagerSingleton *) sharedInstance {
+    
+    NSLog (@"sharedInstance called.");
+    
+    if (nil != mainDataManager) return mainDataManager;
+    static dispatch_once_t pred;        // lock
+    dispatch_once(&pred, ^{             // this code is at most once
+        mainDataManager = [[DataManagerSingleton alloc] init];
+    });
+    
+    return mainDataManager;
+    
+}
 
 @end
