75d53f8f9b731c06657f13a66410154c4cb5cfdd
Codier <pendyaaa@gmail.com>
2012-04-17 23:25:00 -0500

    Location look up async WIP

--- MappMe/CoordinateLookupManager.m
+++ MappMe/CoordinateLookupManager.m
@@ -11,7 +11,8 @@
 
 #import "CoordinateLookupManager.h"
 #import "DebugLog.h"
-#import "AFJSONRequestOperation.h"
+#import "AFHTTPRequestOperation.h"
+#import "CoordPairsHelper.h"
 
 static CoordinateLookupManager *coordinateLookupManager = nil;
 
@@ -43,7 +44,7 @@ static CoordinateLookupManager *coordinateLookupManager = nil;
 /*
  * This method returns formatted query url to Google map
  */
-- (NSURL *)urlWithLookup:(NSString *)lookup
+- (NSURL *)buildUrl:(NSString *)lookup
 {
     NSString *escapedLookup = [lookup stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
     NSString *urlString = [NSString stringWithFormat:@"http://maps.google.com/maps/geo?q=%@&output=csv", escapedLookup];
@@ -51,54 +52,45 @@ static CoordinateLookupManager *coordinateLookupManager = nil;
     return url;
 }
 
-- (CoordPairsHelper *)lookupString:(NSString*)lookup{
-    /*Test CoordPairs for NULL to know lookup failed*/
-    CoordPairsHelper * location;
+-(CoordPairsHelper*) parseResponse:(NSString*)responseStr 
+{
+
+    //TODO add more robust response by understanding error code from Google Map
+    NSArray *listItems = [responseStr componentsSeparatedByString:@","];
+    CoordPairsHelper* location;
+    if([listItems count] >= 4 && [[listItems objectAtIndex:0] isEqualToString:@"200"]) {
+        NSString *latitude = [listItems objectAtIndex:2];
+        NSString *longitude = [listItems objectAtIndex:3];
+        location = [[CoordPairsHelper alloc] initWithLat:latitude andLong:longitude];
+        return location;
+    }
+    return nil;
     
-    NSURL *url = [self urlWithLookup:lookup];
+}
+
+//successCB:(void (^)())successCB failureCB:(void (^)(NSError *error))failureCB
+- (void)lookupLocation:(NSString*)locationStr  {
+    NSURL *url = [self buildUrl:locationStr];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    NSLog(@"Look up %@", lookup);
-    AFJSONRequestOperation *operation = [AFJSONRequestOperation
-                                         JSONRequestOperationWithRequest:request
-                                         success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
-                                             NSLog(@"Success");
-                                             
-                                         } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
-                                             NSLog(@"Failed");
-                                         }];
-    operation.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", nil];
-    [queue addOperation:operation];
-
-
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation , id responseObject) {
+        CoordPairsHelper* location = [self parseResponse:operation.responseString];
+        if (location !=nil) {
+            
+        } else {
+            
+        }
+     } failure:^(AFHTTPRequestOperation *operation , NSError *error) {
+         NSLog(@"Failed: %@", error.localizedDescription);
+     }];
     
-////    DebugLog(@"lookup\n\t %@", urlString);
-//    while(TRUE){
-//        NSString *locationString = [NSString stringWithContentsOfURL:[NSURL URLWithString:urlString] encoding:NSASCIIStringEncoding error:nil];
-//        NSArray *listItems = [locationString componentsSeparatedByString:@","];
-//        
-//        if([listItems count] >= 4 && [[listItems objectAtIndex:0] isEqualToString:@"200"]) {
-//            NSString *latitude = [listItems objectAtIndex:2];
-//            NSString *longitude = [listItems objectAtIndex:3];
-//            location = [[CoordPairsHelper alloc] initWithLat:latitude andLong:longitude];
-//            return location;
-//        }else{
-//            numTries++;
-//            if(numTries>=12){
-////                DebugLog(@"Failed finding Coords for Lookup String: \n\t %@",urlString);
-//                return location;
-//            } 
-//        }
-//    }
-    return location;
+    //operation.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", @"text/plain", nil];
+    [queue addOperation:operation];
 }
 
 
 #pragma mark - public class Methods
 - (CoordPairsHelper *)manageCoordLookupForPlace:(NSString *)lookupString{
-    
-    
-    
-    
     return [[CoordPairsHelper alloc]initWithLat:@"500" andLong:@"600"];
 }
 /*returns coordinate pair from google, nil if none found*/
@@ -110,45 +102,45 @@ static CoordinateLookupManager *coordinateLookupManager = nil;
     }
     
     //DebugLog(@"trying map lookup : %@ , type: %@, city: %@",placeName,schoolType,[supInfo objectForKey: @"city"]);
-    CoordPairsHelper *returnCoords = [self lookupString:lookup];
-    if(returnCoords!= nil){
-        //return returnCoords;
-        //*****Returning here found to give bad results, add location type name for first lookup
-    }
-    //Try name with attached tag, ex. "High School", since Horace Mann does not have "High School" in name, map api cannot find it
-    if ([placeName rangeOfString:schoolType].location == NSNotFound){
-        //DebugLog(@"string does not contain type : %@", schoolType);
-        //lookup = [NSString stringWithFormat:@"%@ %@",placeName,schoolType];
-        //returnCoords = [self lookupString:lookup];
-    } 
-    //Try adding city and state info
-    if ([supInfo objectForKey:@"city"]){
-        lookup=[NSString stringWithFormat:@"%@, %@",lookup,[supInfo objectForKey:@"city"]];
-    }
-    if ([supInfo objectForKey:@"state"]){lookup=[NSString stringWithFormat:@"%@,%@",lookup,[supInfo objectForKey:@"state"]];
-    }
-    if ([supInfo objectForKey:@"state"]){lookup=[NSString stringWithFormat:@"%@,%@",lookup,[supInfo objectForKey:@"country"]];
-    }
-    returnCoords = [self lookupString:lookup];
-    if(returnCoords!= nil){
-        return returnCoords;
-    }
-    
-    
-    //Try 4
-    //remove place name and just use city
-    lookup= @"";
-    if ([supInfo objectForKey:@"city"]){lookup=[NSString stringWithFormat:@"%@",[supInfo objectForKey:@"city"]];}
-    if ([supInfo objectForKey:@"state"]){lookup=[NSString stringWithFormat:@"%@,%@",lookup,[supInfo objectForKey:@"state"]];}
-    if ([supInfo objectForKey:@"state"]){lookup=[NSString stringWithFormat:@"%@,%@",lookup,[supInfo objectForKey:@"country"]];}
-    
-    returnCoords = [self lookupString:lookup];
-    if(returnCoords!= nil){
-        return returnCoords;
-    }
-    
-    /* If no coord found, return nil*/
-    DebugLog(@"Did Not Find:  %@", placeName);
+    [self lookupLocation:lookup];
+//    if(returnCoords!= nil){
+//        //return returnCoords;
+//        //*****Returning here found to give bad results, add location type name for first lookup
+//    }
+//    //Try name with attached tag, ex. "High School", since Horace Mann does not have "High School" in name, map api cannot find it
+//    if ([placeName rangeOfString:schoolType].location == NSNotFound){
+//        //DebugLog(@"string does not contain type : %@", schoolType);
+//        //lookup = [NSString stringWithFormat:@"%@ %@",placeName,schoolType];
+//        //returnCoords = [self lookupString:lookup];
+//    } 
+//    //Try adding city and state info
+//    if ([supInfo objectForKey:@"city"]){
+//        lookup=[NSString stringWithFormat:@"%@, %@",lookup,[supInfo objectForKey:@"city"]];
+//    }
+//    if ([supInfo objectForKey:@"state"]){lookup=[NSString stringWithFormat:@"%@,%@",lookup,[supInfo objectForKey:@"state"]];
+//    }
+//    if ([supInfo objectForKey:@"state"]){lookup=[NSString stringWithFormat:@"%@,%@",lookup,[supInfo objectForKey:@"country"]];
+//    }
+//    returnCoords = [self lookupString:lookup];
+//    if(returnCoords!= nil){
+//        return returnCoords;
+//    }
+//    
+//    
+//    //Try 4
+//    //remove place name and just use city
+//    lookup= @"";
+//    if ([supInfo objectForKey:@"city"]){lookup=[NSString stringWithFormat:@"%@",[supInfo objectForKey:@"city"]];}
+//    if ([supInfo objectForKey:@"state"]){lookup=[NSString stringWithFormat:@"%@,%@",lookup,[supInfo objectForKey:@"state"]];}
+//    if ([supInfo objectForKey:@"state"]){lookup=[NSString stringWithFormat:@"%@,%@",lookup,[supInfo objectForKey:@"country"]];}
+//    
+//    returnCoords = [self lookupString:lookup];
+//    if(returnCoords!= nil){
+//        return returnCoords;
+//    }
+//    
+//    /* If no coord found, return nil*/
+//    DebugLog(@"Did Not Find:  %@", placeName);
     return nil;
 }
 
