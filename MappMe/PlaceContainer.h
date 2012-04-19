996abd76416b1a056cd8eb44eae910d046400518
Codier <pendyaaa@gmail.com>
2012-04-19 04:27:43 -0500

    Remove codes, clean up

--- MappMe/PlaceContainer.h
+++ MappMe/PlaceContainer.h
@@ -21,21 +21,4 @@
 //Get all the places that has someone use it as their given locType.
 //e.g. Get  all places with someone using this place as their home town etc.
 -(NSArray*) getPlacesUsedAs:(locTypeEnum)locType;
-
-
-
-
-///*Adding (place,id) pair to mapping */
-//-(void)addId:(NSString *)placeId andPlaceName:(NSString *)placeName;
-//-(void)addCoordsLat:(NSString *)lat andLong:(NSString *)lon forPlaceId:(NSString *)placeId;
-//-(void)doCoordLookupAndSet:(NSString*)place_id withDict:(NSDictionary *)loc andTypeString:(NSString *)placeTypeName;
-//
-///*lookup methods*/
-//-(NSString *)getPlaceNameFromId:(NSString *)placeId;
-//-(NSString *)getIdFromPlace:(NSString *)placeName;
-//-(Place *)getPlaceFromId:(NSString *)placeId;
-//
-//
-///*  Debug */
-//-(NSUInteger)getNumPlaces;
 @end
