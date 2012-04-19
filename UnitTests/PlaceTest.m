89442b53b514fab6be3b77bb0cee3892044d024d
Codier <pendyaaa@gmail.com>
2012-04-18 21:47:15 -0500

    WIP

---
+++ UnitTests/PlaceTest.m
@@ -0,0 +1,13 @@
+//
+//  PlaceTest.m
+//  MappMe
+//
+//  Created by Di Peng on 4/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PlaceTest.h"
+
+@implementation PlaceTest
+
+@end
