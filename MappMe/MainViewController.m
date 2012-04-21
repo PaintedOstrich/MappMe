e2b8f079f7e115d64ab01448c93c1dbc3a71272f
Codier <pendyaaa@gmail.com>
2012-04-20 22:59:18 -0500

    allow all rotation in MainView Controller

--- MappMe/MainViewController.m
+++ MappMe/MainViewController.m
@@ -65,7 +65,7 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
 - (void)didReceiveMemoryWarning
